4a7dc30abd7b8b9981030b1bea166eb71a531214
croueAlec <alec.croue.pro@gmail.com>
2026-07-24 16:39:35 +0200

    style : apply formatter

--- srcs/arglib/arglib_defines.h
+++ srcs/arglib/arglib_defines.h
@@ -3,10 +3,10 @@
 
 #include "arglib.h"
 
-const char				*is_cli_flag_handler_valid(const cli_flag_handler *flags);
-bool					set_double_dash_separator();
-size_t					get_flag_list_length();
-cli_context				*get_first_ctx_node(cli_context *ctx);
-flag_handler_function	get_str_flag_handler(size_t flag_list_length);
+const char			 *is_cli_flag_handler_valid(const cli_flag_handler *flags);
+bool				  set_double_dash_separator();
+size_t				  get_flag_list_length();
+cli_context			 *get_first_ctx_node(cli_context *ctx);
+flag_handler_function get_str_flag_handler(size_t flag_list_length);
 
 #endif

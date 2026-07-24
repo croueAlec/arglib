1da5243cd575fc259c4566c2ebd3542798940ba4
croueAlec <alec.croue.pro@gmail.com>
2026-07-24 16:36:56 +0200

    style : apply formatter

--- srcs/arglib/arglib_utils.c
+++ srcs/arglib/arglib_utils.c
@@ -1,16 +1,14 @@
 #include "arglib.h"
 #include "arglib_defines.h"
 
-
-const char	*is_cli_flag_handler_valid(const cli_flag_handler *flags)
+const char *is_cli_flag_handler_valid(const cli_flag_handler *flags)
 {
 	if (flags == NULL)
 		return ("Error : The 'flags' may not be NULL"); // flags array must exist
 
-	bool	str_flag_handler_found = false;
-	size_t	i = 0;
-	while (flags[i].handler)
-	{
+	bool   str_flag_handler_found = false;
+	size_t i = 0;
+	while (flags[i].handler) {
 		if (flags[i].metadata.long_flag == NULL || flags[i].metadata.desc == NULL) {
 			return ("Error : Flags cannot contain NULL strings"); // only the last flag may have NULL strings
 		}
@@ -41,9 +39,8 @@ bool set_double_dash_separator()
 
 size_t get_flag_list_length()
 {
-	size_t	i = 0;
-	while (flags[i].handler)
-	{
+	size_t i = 0;
+	while (flags[i].handler) {
 		i++;
 	}
 	return (i);
@@ -58,9 +55,8 @@ cli_context *get_first_ctx_node(cli_context *ctx)
 
 flag_handler_function get_str_flag_handler(size_t flag_list_length)
 {
-	size_t	i = 0;
-	while (i < flag_list_length)
-	{
+	size_t i = 0;
+	while (i < flag_list_length) {
 		if (flags[i].metadata.required_arg == STR_FLAG_ARG)
 			return (flags[i].handler);
 		i++;
